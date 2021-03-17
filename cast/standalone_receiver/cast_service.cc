b3b6a01c06bdc9ad82a5707bd39b64c6490a965c
btolsch <btolsch@chromium.org>
2021-03-17 11:47:31 -0700

    Fix standalone receiver publishing
    This change makes the IP address record the standalone receiver
    publishes consistent with the one on which it's listening.  It still
    prioritizes listening on IPv4 addresses, but previously it might also
    advertise an IPv6 address via mDNS.
    
    Bug: None
    Change-Id: Ic9496d63e7c075b2e2dab708042dfeec963e5337
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/2765985
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>

--- cast/standalone_receiver/cast_service.cc
+++ cast/standalone_receiver/cast_service.cc
@@ -38,8 +38,7 @@ discovery::Config MakeDiscoveryConfig(const InterfaceInfo& interface) {
       discovery::Config::NetworkInfo::kNoAddressFamily;
   if (interface.GetIpAddressV4()) {
     supported_address_families |= discovery::Config::NetworkInfo::kUseIpV4;
-  }
-  if (interface.GetIpAddressV6()) {
+  } else if (interface.GetIpAddressV6()) {
     supported_address_families |= discovery::Config::NetworkInfo::kUseIpV6;
   }
   config.network_info.push_back({interface, supported_address_families});
